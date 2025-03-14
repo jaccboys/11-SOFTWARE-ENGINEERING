2aa071dd48f30927501a0778870c825fe48d7727
JACK <jboys@student.saintaug.nsw.edu.au>
2025-03-14 14:12:55 +1100

    bug fixing the filter_by_topic function

--- quiz assesment/assesment.py
+++ quiz assesment/assesment.py
@@ -30,18 +30,24 @@ def print_questions(questions): #function to print out the questions
 
 def filter_by_topic(questions): #function to filter the questions based on the player's choice
     typewriter('What topic would you like to be quizzed on?')
-    typewriter("Enter your chosen topic from the following: \n Ninjago | Star wars") 
+    typewriter("Enter your chosen topic from the following: \n Ninjago \n Star wars") 
+    
     player_choice = input().lower().strip()
+    
     print('testing', player_choice) #testing
     #VERIFICATION FOR ANSWER
-    if player_choice != 'ninjago' and player_choice != 'star wars':
+    
+    while player_choice != 'ninjago' and player_choice != 'star wars':
         typewriter("Your input is invalid. \n please enter a valid input. (make sure your spelling is correct)") 
         player_choice = input().lower().strip()
 
+    
+    
     filtered_questions = []
     for i in questions:
-        if i["topic"] == player_choice:
+        if i["topic"].lower() == player_choice.lower():
             filtered_questions.append(i)
+
     print('testing', filtered_questions) #TESTING
     return filtered_questions
 
@@ -51,20 +57,19 @@ def filter_by_topic(questions): #function to filter the questions based on the p
 
 def game_loop(filtered_questions):
     score = 0
-    for x in filtered_questions:
+    for x in filtered_questions: 
         typewriter(x["qns"])
         typewriter("Enter your answer: ")
-        answer = input().lower().strip()
-        if answer == x["Correct Ans"]:
-            typewriter("Correct!")
-            score += 1
+        answer = input().lower().strip() #recieve lowercase input without trailing spaces
+        if answer == x["Correct Ans"]: #check if the answer is correct
+            typewriter("Correct!") #print correct if the answer is correct
+            score += 1 #increment the score by 1
         else:
             typewriter("Wrong!")
     return score
 
 def main(questions):
     welcome()
-    filter_by_topic(questions)
     filtered_questions = filter_by_topic(questions)
     
 main(questions)
