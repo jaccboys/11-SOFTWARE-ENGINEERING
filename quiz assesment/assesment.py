a7e489f1225beae52380c944c96fc7629d7083f4
JACK <jboys@student.saintaug.nsw.edu.au>
2025-03-17 10:25:14 +1100

    updating the dialogue to the user

--- quiz assesment/assesment.py
+++ quiz assesment/assesment.py
@@ -9,12 +9,12 @@ def typewriter(words): #function to print out text in a typewriter fashion
         sleep(0.03)
         sys.stdout.write(char)
         sys.stdout.flush()
-        if char in ':.!f, ':
+        if char in ':.!, ?><':
             sleep(0.06)
     sys.stdout.write("\n\n")
 
 def welcome(): #function to print out the welcome message
-    typewriter("WELCOME TO THE QUIZ GAME.")
+    typewriter("Welcome to my quiz game!")
     typewriter("You will be asked a series of extreamly difficult questions.")
     typewriter("You must answer them correctly to win.")
     typewriter("Good luck!")
@@ -25,7 +25,7 @@ def welcome(): #function to print out the welcome message
 
 def filter_by_topic(questions): #function to filter the questions based on the player's choice
     typewriter('What topic would you like to be quizzed on?')
-    typewriter("Enter your chosen topic from the following: \n Ninjago \n Star wars") 
+    typewriter("Enter your chosen topic from the following:\n Ninjago | Star wars") 
     
     player_choice = input().lower().strip()
     
@@ -36,6 +36,8 @@ def filter_by_topic(questions): #function to filter the questions based on the p
         typewriter("Your input is invalid. \n please enter a valid input. (make sure your spelling is correct)") 
         player_choice = input().lower().strip()
     
+    typewriter('You have chosen ' + player_choice + ' as your topic.')
+
     filtered_questions = []
     for i in questions:
         if i["topic"].lower() == player_choice.lower():
@@ -46,21 +48,24 @@ def filter_by_topic(questions): #function to filter the questions based on the p
 
 def game_loop(filtered_questions):
     score = 0
-    typewriter("Remember to spell your answers correctly please.")
+    typewriter("**Remember to spell your answers correctly please.**")
+    typewriter('please type out one of each answer from the options given')
     for x in filtered_questions: 
         typewriter(x["qns"])
-        typewriter(x['options'])
-        typewriter("Enter your answer below: \n\n >>")
+        typewriter(", ".join(x['options']))
+        typewriter("Enter your answer below:")
         answer = input()
         if answer.lower() == x["Correct Ans"]: #check if the answer is correct
-            typewriter("Correct!") #print correct if the answer is correct
+            typewriter("\nCorrect!") #print correct if the answer is correct
             score += 1 #increment the score by 1
         else:
-            typewriter("Wrong!")
+            typewriter("\nWrong!")
     return score
 
 def end_game(score, filtered_questions):
     typewriter(f"Your score is {score} out of {len(filtered_questions)} questions.")
+    typewriter("Thank you for playing!")
+    typewriter("Hope you had fun!")
     
 
 
