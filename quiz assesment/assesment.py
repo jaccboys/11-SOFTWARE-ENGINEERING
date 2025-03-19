b24fe2331224711ce8d0aa249c64c8011e8df890
JACK <jboys@student.saintaug.nsw.edu.au>
2025-03-20 10:11:09 +1100

    attempting to add hints

--- quiz assesment/assesment.py
+++ quiz assesment/assesment.py
@@ -45,14 +45,35 @@ def game_loop(filtered_questions):
     score = 0
     typewriter("**Remember to spell your answers correctly please.**")
     typewriter('please type out one of each answer from the options given')
+    typewriter("If your having trouble, remember you can ask for a hint by typing 'hint'")
+    typewriter("Beware, you only have 3")
     typewriter('Good luck!')
 
     for x in filtered_questions: 
         typewriter(x["qns"])
         typewriter(" | ".join(x['options']))
         typewriter("Enter your answer below:")
-
         answer = input().strip().lower()
+        correct_answer = x["Correct Ans"].strip().lower()
+
+        #HINTS
+        hints = 3
+        if answer == "hint":
+            if hints == 0:
+                typewriter("Sadly you have no hints left.")
+                typewriter("I appologise but you have no hints left.")
+            else:
+                typewriter("Here is your hint,")
+                typewriter(x["hint"])
+                hints -= 1
+                if hints == 0:
+                    typewriter("You have no hints left.")
+                else:
+                    typewriter(f"remember though, you have {hints} hints left.")
+        typewriter("Please re-enter your answer")
+        answer = input().strip().lower()
+
+
         correct_answer = x["Correct Ans"].strip().lower()
         valid_answers = [option.strip().lower() for option in x['options']] #creating a seperate list of options that are lowercased and no trailing spaces
         while answer not in valid_answers: #checking that the answer is on of the options available
